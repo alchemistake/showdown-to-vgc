d7ed401c08677856ae0bd6cafe4753b39f47ff8a
Kasper Wissendorf <kw2011@live.dk>
2026-07-28 10:30:42 +0200

    refactor: remove unused calcStatOriginal function for cleaner code

--- src/utils/stat-calc.ts
+++ src/utils/stat-calc.ts
@@ -1,27 +1,6 @@
 import type { StatID } from '@pkmn/data';
 import natureCalc from './nature-calc';
 
-// export function calcStatOriginal(
-//   stat: StatID,
-//   baseStat: number,
-//   iv: number,
-//   ev: number,
-//   level: number,
-//   nature: string,
-// ): number {
-//   if (stat === 'hp') {
-//     if (baseStat === 1) return 1;
-
-//     return Math.floor(((2 * baseStat + iv + Math.floor(ev / 4)) * level) / 100) + level + 10;
-//   } else {
-//     const natureMultiplierValue = natureCalc(stat, nature);
-//     return Math.floor(
-//       (Math.floor(((2 * baseStat + iv + Math.floor(ev / 4)) * level) / 100) + 5) *
-//         natureMultiplierValue,
-//     );
-//   }
-// }
-
 export function calcStatChampions(
   stat: StatID,
   baseStat: number,
